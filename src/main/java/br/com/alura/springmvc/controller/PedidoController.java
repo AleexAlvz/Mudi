b4894329ea44c690480d5b4e5811e200e0fbd87b
AleexAlvz <61116687+AleexAlvz@users.noreply.github.com>
2021-03-10 19:54:19 -0300

    Interface para novos produtos melhorada, mas com bug na validação

--- src/main/java/br/com/alura/springmvc/controller/PedidoController.java
+++ src/main/java/br/com/alura/springmvc/controller/PedidoController.java
@@ -1,17 +1,34 @@
 package br.com.alura.springmvc.controller;
-
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import br.com.alura.springmvc.dto.RequisicaoNovoPedido;
+import br.com.alura.springmvc.model.Pedido;
+import br.com.alura.springmvc.repository.PedidoRepository;
 
 @Controller
-@RequestMapping("pedido")
+@RequestMapping("/pedido")
 public class PedidoController {
 	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
 	@GetMapping("/formulario")
-	public String formulario() {
+	public String formulario(RequisicaoNovoPedido requisicao) {
+		return "pedido/formulario";
+	}
+
+	@PostMapping("/novo")
+	public String novo(@Valid RequisicaoNovoPedido requisicao, BindingResult result) {
+		
+		Pedido pedido = requisicao.toPedido();
+		pedidoRepository.save(pedido);
+		
 		return "pedido/formulario";
 		
 	}
-
 }
